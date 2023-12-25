04d223b9d5ae48384c555fcbc5d6265c1d5b9262
chengfengrui <chengfengrui@123>
2023-12-25 16:10:02 +0800

    feature: 无脱敏用户信息查询

--- shortlink-admin/src/main/java/com/fengrui/shortlink/admin/service/UserService.java
+++ shortlink-admin/src/main/java/com/fengrui/shortlink/admin/service/UserService.java
@@ -2,8 +2,11 @@ package com.fengrui.shortlink.admin.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.fengrui.shortlink.admin.dao.entity.UserDO;
+import com.fengrui.shortlink.admin.dto.resp.UserActualRespDTO;
 import com.fengrui.shortlink.admin.dto.resp.UserRespDTO;
 
 public interface UserService extends IService<UserDO> {
     public UserRespDTO getUserByUsername(String username);
+
+    public UserActualRespDTO getActualUserByUsername(String username);
 }
