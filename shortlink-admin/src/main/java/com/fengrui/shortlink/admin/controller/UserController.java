03e49a57f1b4745a1389aae350d166ee4c77236e
chengfengrui <chengfengrui@123>
2023-12-26 16:56:50 +0800

    feature: 实现用户注册功能，redis分布式锁避免毫秒级大量相同用户名注册

--- shortlink-admin/src/main/java/com/fengrui/shortlink/admin/controller/UserController.java
+++ shortlink-admin/src/main/java/com/fengrui/shortlink/admin/controller/UserController.java
@@ -2,16 +2,15 @@ package com.fengrui.shortlink.admin.controller;
 
 import com.fengrui.shortlink.admin.common.convention.result.Result;
 import com.fengrui.shortlink.admin.common.convention.result.Results;
+import com.fengrui.shortlink.admin.dto.req.UserRegisterReqDTO;
 import com.fengrui.shortlink.admin.dto.resp.UserActualRespDTO;
 import com.fengrui.shortlink.admin.dto.resp.UserRespDTO;
 import com.fengrui.shortlink.admin.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 @Tag(name = "用户管理")
 @RestController
 @RequiredArgsConstructor
@@ -24,7 +23,7 @@ public class UserController {
      * 根据用户名查询用户信息
      */
     @GetMapping("/users")
-    @Operation(summary = "根据用户名查找")
+    @Operation(summary = "根据用户名查询脱敏用户信息")
     public Result<UserRespDTO> getUserByUsername(@RequestParam("username") String username){
         return Results.success(userService.getUserByUsername(username));
     }
@@ -42,8 +41,18 @@ public class UserController {
      * 判断用户名是否存在
      */
     @GetMapping("/users/has-username")
-    @Operation(summary = "根据用户名查询无脱敏用户信息")
+    @Operation(summary = "判断用户是否存在")
     public Result<Boolean> hasUsername(@RequestParam("username") String username){
-        return Results.success(userService.hasUserName(username));
+        return Results.success(userService.hasUsername(username));
+    }
+
+    /**
+     * 注册用户
+     */
+    @PostMapping("/users")
+    @Operation(summary = "注册用户")
+    public Result<Void> register(@RequestBody UserRegisterReqDTO registerReqDTO){
+        userService.register(registerReqDTO);
+        return Results.success();
     }
 }
--- shortlink-admin/src/main/java/com/fengrui/shortlink/admin/service/UserService.java
+++ shortlink-admin/src/main/java/com/fengrui/shortlink/admin/service/UserService.java
@@ -2,13 +2,32 @@ package com.fengrui.shortlink.admin.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.fengrui.shortlink.admin.dao.entity.UserDO;
+import com.fengrui.shortlink.admin.dto.req.UserRegisterReqDTO;
 import com.fengrui.shortlink.admin.dto.resp.UserActualRespDTO;
 import com.fengrui.shortlink.admin.dto.resp.UserRespDTO;
 
 public interface UserService extends IService<UserDO> {
+
+    /**
+     * 根据用户名查询用户信息
+     * @param username
+     * @return 用户实体类
+     */
     public UserRespDTO getUserByUsername(String username);
 
+    /**
+     * 根据用户名查询用户真实信息
+     * @param username
+     * @return 用户实体类
+     */
     public UserActualRespDTO getActualUserByUsername(String username);
 
-    public Boolean hasUserName(String username);
+    /**
+     * 判断用户名是否存在
+     * @param username
+     * @return 存在返回True，不存在返回False
+     */
+    public Boolean hasUsername(String username);
+
+    public void register(UserRegisterReqDTO userRegisterReqDTO);
 }
