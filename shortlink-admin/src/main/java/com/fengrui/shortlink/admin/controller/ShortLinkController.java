e703a1ecb87e81ab8d3c41471eef2aa0b004e203
chengfengrui <chengfengrui@123>
2024-01-11 15:21:26 +0800

    feature：后管系统联调中台，查询短链接分组时计算分组下短链接数量

--- shortlink-admin/src/main/java/com/fengrui/shortlink/admin/controller/ShortLinkController.java
+++ shortlink-admin/src/main/java/com/fengrui/shortlink/admin/controller/ShortLinkController.java
@@ -5,6 +5,7 @@ import com.fengrui.shortlink.admin.remote.ShortLinkRemoteService;
 import com.fengrui.shortlink.admin.remote.dto.req.ShortLinkCreateReqDTO;
 import com.fengrui.shortlink.admin.remote.dto.req.ShortLinkPageReqDTO;
 import com.fengrui.shortlink.admin.remote.dto.resp.ShortLinkCreateRespDTO;
+import com.fengrui.shortlink.admin.remote.dto.resp.ShortLinkGroupCountQueryRespDTO;
 import com.fengrui.shortlink.admin.remote.dto.resp.ShortLinkPageRespDTO;
 import com.fengrui.shortlink.common.convention.result.Result;
 import io.swagger.v3.oas.annotations.Operation;
@@ -12,6 +13,8 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Tag(name = "短链接链接管理")
 @RestController
@@ -35,7 +38,9 @@ public class ShortLinkController {
      * 分页查询短链接
      */
     @PostMapping("/links/page")
+    @Operation(summary = "短链接分页查询")
     public Result<IPage<ShortLinkPageRespDTO>> pageShortLink(@RequestBody ShortLinkPageReqDTO requestParam) {
         return shortLinkRemoteService.pageShortLink(requestParam);
     }
+
 }
--- shortlink-admin/src/main/java/com/fengrui/shortlink/admin/remote/ShortLinkRemoteService.java
+++ shortlink-admin/src/main/java/com/fengrui/shortlink/admin/remote/ShortLinkRemoteService.java
@@ -9,10 +9,12 @@ import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.fengrui.shortlink.admin.remote.dto.req.ShortLinkCreateReqDTO;
 import com.fengrui.shortlink.admin.remote.dto.req.ShortLinkPageReqDTO;
 import com.fengrui.shortlink.admin.remote.dto.resp.ShortLinkCreateRespDTO;
+import com.fengrui.shortlink.admin.remote.dto.resp.ShortLinkGroupCountQueryRespDTO;
 import com.fengrui.shortlink.admin.remote.dto.resp.ShortLinkPageRespDTO;
 import com.fengrui.shortlink.common.convention.result.Result;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -51,4 +53,15 @@ public interface ShortLinkRemoteService {
         return JSON.parseObject(resultPageStr, new TypeReference<>() {
         });
     }
+
+    default Result<List<ShortLinkGroupCountQueryRespDTO>> listGroupShortLinkCount(List<String> gids) {
+        String gidsParam = String.join(",", gids);
+        String url = String.format("http://127.0.0.1:8001/api/short-link/project/links/count?gids=%s", gidsParam);
+        HttpResponse httpResponse = HttpUtil.createGet(url)
+                .contentType(ContentType.JSON.toString())
+                .execute();
+        String resultPageStr = httpResponse.body();
+        return JSON.parseObject(resultPageStr, new TypeReference<>() {});
+    }
+
 }
