7a5aa161c1e935ae9923af46313440a36d9fbd6d
chengfengrui <chengfengrui@123>
2024-02-14 18:35:54 +0800

    optimize：优化短链接redis相关key规范

--- shortlink-admin/src/main/java/com/fengrui/shortlink/admin/common/biz/user/UserTransmitFilter.java
+++ shortlink-admin/src/main/java/com/fengrui/shortlink/admin/common/biz/user/UserTransmitFilter.java
@@ -17,6 +17,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.List;
 
+import static com.fengrui.shortlink.common.constant.RedisCacheConstant.USER_LOGIN_KEY;
 import static com.fengrui.shortlink.common.convention.errorcode.BaseErrorCode.USER_TOKEN_FAIL;
 
 /**
@@ -48,7 +49,7 @@ public class UserTransmitFilter implements Filter {
             }
             Object userInfoJsonStr;
             try {
-                userInfoJsonStr = stringRedisTemplate.opsForHash().get("login_" + username, token);
+                userInfoJsonStr = stringRedisTemplate.opsForHash().get(USER_LOGIN_KEY + username, token);
                 if (userInfoJsonStr == null) {
                     throw new ClientException(USER_TOKEN_FAIL);
                 }
